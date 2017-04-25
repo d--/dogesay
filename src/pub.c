195609b5402fa28872ed665f4e8342fd3bc31176
d-- <d--@users.noreply.github.com>
2017-04-25 15:37:49 -0400

    publish by reading from stdin, multicast

--- src/pub.c
+++ src/pub.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <lcm/lcm.h>
 
 #include "doge_say_t.h"
 
+#define MULTICAST_LAN "udpm://239.255.76.67:7667?ttl=1"
+
 int main(int argc, char **argv)
 {
-    lcm_t *lcm = lcm_create(NULL);
+    lcm_t *lcm = lcm_create(MULTICAST_LAN);
     if (!lcm) {
         return 1;
     }
@@ -16,7 +19,13 @@ int main(int argc, char **argv)
         .phrase = "yeah buddy",
     };
 
-    doge_say_t_publish(lcm, "DOGESAY", &dogesay);
+    char buf[1024];
+    fgets(buf, 1024, stdin);
+    while (1) {
+        dogesay.phrase = buf;
+        doge_say_t_publish(lcm, "DOGESAY", &dogesay);
+        fgets(buf, 1024, stdin);
+    }
 
     lcm_destroy(lcm);
     return 0;
