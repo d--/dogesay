6cc634a8f8b4e2a69b2e61fe956992b4c8eabcef
d-- <d--@users.noreply.github.com>
2018-09-12 15:27:02 -0400

    Remove misleading script

--- scripts/compile_lcm.sh
+++
@@ -1,7 +0,0 @@
-if [[ $(docker images -q dogesay:latest) == "" ]]; then 
-  docker build -t dogesay .    
-fi
-
-echo $PWD
-
-docker run --rm -v $PWD:/home/dogesay -w=/home/dogesay dogesay /bin/bash -c "cd src && lcm-gen -c say.lcm"
